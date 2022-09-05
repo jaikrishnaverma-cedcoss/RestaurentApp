6460c900010026506467b31731c8cca2a0e1c7b1
jaikrishnaverma <jaikrishnaverma>
2022-09-05 12:20:57 +0530

    Login/Signup

---
+++ src/components/master/SearchBox.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class SearchBox extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    state = {  }
+    render() { 
+        return ( 
+            <>
+            
+            </>
+         );
+    }
+}
+ 
+export default SearchBox;
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return ( 
+            <>
+            Home
+            </>
+         );
+    }
+}
+ 
+export default Home;
